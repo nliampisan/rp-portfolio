59902f2b9c85ddea0e6aba273baa704c56dfe60e
nliampisan <nliampisan@gmail.com>
2022-04-20 01:06:03 +0700

    moved test_ai and add database objects

--- test_ai.py
+++ test_ai.py
@@ -3,19 +3,36 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import Client
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-from .models import *
+from blog.models import *
+from projects.models import *
 from selenium.webdriver import ActionChains
 # https://www.geeksforgeeks.org/action-chains-in-selenium-python/
 import time
+import datetime
 from django.contrib.auth import authenticate
-
 
 class MyTests(StaticLiveServerTestCase):
 
     def setUp(self):
+        self.patient = User.objects.create_user(first_name="first", last_name="last",
+                                                email='patient@example.com', password="password",
+                                                username='patient@example.com')
+        self.patient.save()
+
+        self.project = Project.objects.create(title="Project1", description="first project", technology="Java", image="")
+        self.project.save()
+        self.cat = Category.objects.create(name="Category 1")
+        self.cat.save()
+        self.post = Post.objects.create(title="Post A", body="Post Body")
+        self.post.save()
+        self.post.categories.add(self.cat)
+        self.comment = Comment.objects.create(author="Author", body="Comment Body", post=self.post)
+        self.comment.save()
+
+
         self.driver = webdriver.Chrome(r"C:/Users/nliam/OneDrive/Documents/chromedriver_win32/chromedriver.exe")
         self.client = Client()
-        self.driver.get('%s%s' % (self.live_server_url, '/admin/'))
+        self.driver.get('%s%s' % (self.live_server_url, '/projects/'))
         # link =  self.driver.find_element(by=By.CLASS_NAME, value='navbar-brand');
         # link.click()
         all_links = self.driver.find_elements(by=By.TAG_NAME, value="a")
